cff91db9fa91f0ad87cc552c633af5f654643edc
Vierce1 <a.westermann.19@gmail.com>
2024-07-19 14:09:21 -0500

    getting photo year from filename or exif

--- main.py
+++ main.py
@@ -3,25 +3,56 @@ import json
 import paramiko
 from base64 import decodebytes
 import os
+import exifread
+import re
+import zipfile
 
 
 path_to_photos = r'/home/andweste/AshleyPictures/'
-local_photo_repo = "Photos/"
+local_photo_repo = 'photo_temp/'
+log_file = 'log.txt'
+year_regex = '20[0-2][0-9]'
+
 
 class Server:
     def __init__(self, json_string):
         self.__dict__ = json.loads(json_string)
 
 
-def get_new_photo_names() -> list[str]:
+def get_new_photos():
+    # Compile any new photos added by checking local storage for same name
+    # This is safer than adding any in the last 24h b/c the app may crash at some point
     new_photo_files = []
+    log = open(log_file, 'a')
     dir_contents = sftp.execute(f"cd {path_to_photos} && ls")
     for file in dir_contents:
         file = file.strip().decode('utf-8')
         if file not in os.listdir(local_photo_repo):
             sftp.get(remotepath=f'{path_to_photos}{file}', localpath=f'{local_photo_repo}{file}')
-    print(f'Downloaded {len(new_photo_files)} new photos')
+            new_photo_files.append(file)
+    log.write(f'\nDownloaded {len(new_photo_files)} new photos')
 
+
+def get_exif_year(file_path: str) -> str | None:
+    # First just try to grab the date from the file name
+    year_search = re.search(year_regex, file_path)
+    if year_search:
+        return year_search.group()
+
+    # Didn't have the year in the file name. Try EXIF data
+    file = open(file_path, 'rb')
+    tags = exifread.process_file(file, details=True)#, stop_tag='DateTimeOriginal')
+    for tag, val in tags.items():
+        if "date" in str(tag).lower():
+            year_search = re.search(year_regex, str(val))
+            if year_search:
+                return year_search.group()
+
+    return None
+
+
+
+# def archive_new_photos():
 
 
 
@@ -36,8 +67,6 @@ if __name__ == '__main__':
 
     with pysftp.Connection(server.Host, username=server.User, private_key="private_key.ppk", port=server.Port,
                            cnopts=cnopts) as sftp:
-        new_photo_names = get_new_photo_names()
-
-        # Compile any new photos added by checking local storage for same name
-        # This is safer than adding any in the last 24h b/c the app may crash at some point
-
+        get_new_photos()
+        # archive_new_photos()
+        get_exif_year('photo_temp/ashleyw-2.jpg')
